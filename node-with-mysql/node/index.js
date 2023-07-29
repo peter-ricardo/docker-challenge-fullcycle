a6e8e7fcca2b9cca50ed68acaf9d89463b8f7fe1
Peter Ricardo <peter.ricardo.pfz@gmail.com>
2023-07-29 14:53:40 -0300

    refactor: change mysql lib

--- node-with-mysql/node/index.js
+++ node-with-mysql/node/index.js
@@ -7,18 +7,43 @@ const config = {
     password: 'root',
     database:'nodedb'
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+const mysql = require("mysql2/promise");
+const { faker } = require('@faker-js/faker');
 
-const sql = `INSERT INTO people(name) values('Wesley')`
-connection.query(sql)
-connection.end()
+async function addRecord() {
+    const connection = await mysql.createConnection(config)
 
+    const sql = `INSERT INTO people(name) values("${faker.person.fullName()}")`
+    await connection.query(sql)
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
+    connection.end()
+}
+
+async function getRecords() {
+    const connection = await mysql.createConnection(config);
+    const sql = `SELECT name FROM people`
+    const [rows] = await connection.query(sql);
+    connection.end()
+    return rows || [];
+}
+
+app.get('/', async (req,res) => {
+    try {        
+        const tableRecords = await getRecords();
+        res.send(`<h1>Full Cycle</h1>
+        - Lista de nomes cadastrada no banco de dados (${tableRecords.length}): 
+        ${tableRecords.map((row) => `<p>${row.name}</p>`).join('\n')}
+    `)
+    } catch (error) {
+        console.error(error);
+        res.send(`
+            <h1>Full Cycle</h1>
+            <p>Erro ao buscar nomes no banco de dados</p>
+        `);
+    }
 })
 
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
+    addRecord();
 })
